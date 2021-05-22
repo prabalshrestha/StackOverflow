ac94dc2006c038dc3f50e9dfbb75e1f8def9b98a
Aayush Bista <aayushbikrambista@gmail.com>
2021-05-22 15:26:31 +0545

    trial 2

--- src/main/java/com/et/stackoverflow/trial/trial.java
+++ src/main/java/com/et/stackoverflow/trial/trial.java
@@ -2,4 +2,11 @@ package com.et.stackoverflow.trial;
 
 public class trial {
     // added
+    //git branch
+    //git checkout -b <branch name>
+    //git add .
+    //git commit -m "message"
+    //git  push --set-upstream origin trial
+    //
+
 }
