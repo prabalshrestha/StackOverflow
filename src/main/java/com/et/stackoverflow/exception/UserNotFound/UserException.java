a71044500bb98aaaf96d0aecf66d15544ba6fd4c
Prabal Shrestha <prabalshrestha2@gmail.com>
2021-05-25 10:43:59 +0545

    userid in question,answer

--- src/main/java/com/et/stackoverflow/exception/UserNotFound/UserException.java
+++ src/main/java/com/et/stackoverflow/exception/UserNotFound/UserException.java
@@ -6,13 +6,20 @@ import java.time.ZonedDateTime;
 
 public class UserException {
     private final String message;
+    private final String messageCode;
     private final HttpStatus httpStatus;
+
     private final ZonedDateTime timestamp;
 
-    public UserException(String message,  HttpStatus httpStatus, ZonedDateTime timestamp) {
+    public UserException(String message, String messageCode, HttpStatus httpStatus, ZonedDateTime timestamp) {
         this.message = message;
+        this.messageCode = messageCode;
         this.httpStatus = httpStatus;
         this.timestamp = timestamp;
+    }
+
+    public String getMessageCode() {
+        return messageCode;
     }
 
     public String getMessage() {
